d7655ad24e34607abf82139d8fbe8e03b947ef6f
Audric Ackermann <audric@getsession.org>
2024-07-27 11:34:52 +1000

    bump

--- index.d.ts
+++ index.d.ts
@@ -18,8 +18,8 @@ interface WasmModule {
 type EmbindString = ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string;
 export interface ConfigBase {
   storageNamespace(): Namespace;
-  currentHashes(): vector<string>;
-  mergeHex(_0: vector<MergeStruct>): vector<string>;
+  currentHashes(): StringVector;
+  mergeHex(_0: MergeStructVector): StringVector;
   needsPush(): boolean;
   needsDump(): boolean;
   encryptionDomain(): string;
@@ -28,7 +28,7 @@ export interface ConfigBase {
   dumpHex(): string;
   makeDumpHex(): string;
   push(): PushResultStruct;
-  mergeHexSingle(_0: MergeStruct): vector<string>;
+  mergeHexSingle(_0: MergeStruct): StringVector;
   delete(): void;
 }
 
@@ -47,7 +47,7 @@ export interface UserProfile extends ConfigBase {
 }
 
 export interface Contacts extends ConfigBase {
-  all(): vector<contact_info>;
+  all(): ContactInfoVector;
   set(_0: ContactInfo): void;
   empty(): boolean;
   size(): number;
@@ -129,7 +129,7 @@ export interface ExpirationModeValue<T extends number> {
 }
 export type ExpirationMode = ExpirationModeValue<0>|ExpirationModeValue<1>|ExpirationModeValue<2>;
 
-export interface vector<ustring> {
+export interface UstringVector {
   size(): number;
   get(_0: number): EmbindString | undefined;
   push_back(_0: EmbindString): void;
@@ -138,7 +138,7 @@ export interface vector<ustring> {
   delete(): void;
 }
 
-export interface vector<contact_info> {
+export interface ContactInfoVector {
   push_back(_0: ContactInfo): void;
   resize(_0: number, _1: ContactInfo): void;
   size(): number;
@@ -147,7 +147,7 @@ export interface vector<contact_info> {
   delete(): void;
 }
 
-export interface vector<string> {
+export interface StringVector {
   size(): number;
   get(_0: number): EmbindString | undefined;
   push_back(_0: EmbindString): void;
@@ -156,7 +156,7 @@ export interface vector<string> {
   delete(): void;
 }
 
-export interface vector<MergeStruct> {
+export interface MergeStructVector {
   size(): number;
   get(_0: number): MergeStruct | undefined;
   push_back(_0: MergeStruct): void;
@@ -168,7 +168,7 @@ export interface vector<MergeStruct> {
 export type PushResultStruct = {
   seqno: number,
   dataHex: EmbindString,
-  hashes: vector<string>
+  hashes: StringVector
 };
 
 export type MergeStruct = {
@@ -188,10 +188,10 @@ interface EmbindModule {
   Namespace: {UserProfile: NamespaceValue<2>, Contacts: NamespaceValue<3>, ConvoInfoVolatile: NamespaceValue<4>, UserGroups: NamespaceValue<5>, GroupMessages: NamespaceValue<11>, GroupKeys: NamespaceValue<12>, GroupInfo: NamespaceValue<13>, GroupMembers: NamespaceValue<14>};
   NotificationMode: {All: NotificationModeValue<1>, defaulted: NotificationModeValue<0>, disabled: NotificationModeValue<2>, mentions_only: NotificationModeValue<3>};
   ExpirationMode: {None: ExpirationModeValue<0>, AfterSend: ExpirationModeValue<1>, AfterRead: ExpirationModeValue<2>};
-  vector<ustring>: {new(): vector<ustring>};
-  vector<contact_info>: {new(): vector<contact_info>};
-  vector<string>: {new(): vector<string>};
-  vector<MergeStruct>: {new(): vector<MergeStruct>};
+  UstringVector: {new(): UstringVector};
+  ContactInfoVector: {new(): ContactInfoVector};
+  StringVector: {new(): StringVector};
+  MergeStructVector: {new(): MergeStructVector};
 }
 
 export type MainModule = WasmModule & typeof RuntimeExports & EmbindModule;
